1f428d0fde4e4e76670658b1fbd82b07676347e8
lingjiameng <1574761457@qq.com>
2019-05-21 12:50:43 +0800

    initial

--- dataloader.py
+++ dataloader.py
@@ -22,60 +22,151 @@ def load_data_set(start,end):
     return data,label
 
 
+def load_train_norm():
+    train_data1, train_label1 = load_data_set(1, 4)
+    train_data2, train_label2 = load_data_set(5, 8)
+    
+    train_data2_pos = train_data2[np.squeeze(train_label2==1),:]
+    train_label2_pos = train_label2[train_label2==1][:,np.newaxis]
 
-train_data, train_label = load_data_set(1,8)
-print(train_data.shape)
-print(train_label.shape)
+    data = np.concatenate([train_data1,train_data2_pos])
+    label =np.concatenate([train_label1,train_label2_pos])
+    return data,label
 
-train_set = np.concatenate([train_data,train_label],axis=1)
+def load_test_norm():
+    train_data1, train_label1 = load_data_set(10, 10)
+    train_data2, train_label2 = load_data_set(9, 9)
 
-data_iter = torch.utils.data.DataLoader(train_set,shuffle=True,batch_size=64)
-data_size = len(train_set)
+    train_data2_pos = train_data2[np.squeeze(train_label2 == 1), :]
+    train_label2_pos = train_label2[train_label2 == 1][:, np.newaxis]
 
+    data = np.concatenate([train_data1, train_data2_pos])
+    label = np.concatenate([train_label1, train_label2_pos])
+    return data, label
 
+def train_logistic():
+    # train_data, train_label = load_data_set(1,8)
+    train_data, train_label = load_train_norm()
 
+    print(train_data.shape)
+    print(train_label.shape)
 
-# 随机抽样 和 batch
+    train_set = np.concatenate([train_data,train_label],axis=1)
 
-LR = 0.002
-epoch = 50
+    data_iter = torch.utils.data.DataLoader(train_set,shuffle=True,batch_size=64)
+    data_size = len(train_set)
 
-
-mycls  = mymodel.LogisticClassifier(900,LR)
-
-for e in  range(epoch):
-    e_loss = 0.0
-    e_acc = 0.0
-    for data in data_iter:
-        # print(data.shape)
-        # X = np.ndarray((64,900))
-        # Y = np.ndarray((64,1))
-        X = np.asarray(data[:,:900])
-        Y = np.asarray(data[:,900:])
-
-        loss,acc = mycls.learn(X,Y)
-
-        e_loss += loss
-        e_acc += acc
-
-    e_loss /= data_size
-    e_acc /= data_size
-    print("[epoch:{}][loss:{:.5f}][acc:{:.5f}]".format(e,e_loss,e_acc))
+    # 随机抽样 和 batch
+    # LR = 0.002
+    LR= 0.002
+    epoch = 50
 
 
-# test data
-test_data, test_label = load_data_set(9,10)
+    mycls  = mymodel.LogisticClassifier(900,LR)
 
-X = test_data
+    for e in  range(epoch):
+        e_loss = 0.0
+        e_acc = 0.0
+        for data in data_iter:
+            # print(data.shape)
+            # X = np.ndarray((64,900))
+            # Y = np.ndarray((64,1))
+            X = np.asarray(data[:,:900])
+            Y = np.asarray(data[:,900:])
 
-Y = test_label
-# print(Y)
+            loss,acc = mycls.learn(X,Y)
 
-_Y = mycls.predict(X)
+            e_loss += loss
+            e_acc += acc
 
-error = np.sum(np.abs(Y-_Y)/2)
-print("[correct rate:{:.5f}][test size:{}][error size:{}]".format(1 - error/len(Y),len(Y),error))
+        e_loss /= data_size
+        e_acc /= data_size
+        print("[epoch:{}][loss:{:.5f}][acc:{:.5f}]".format(e,e_loss,e_acc))
 
-plt.plot(mycls.w)
-plt.show()
 
+    # test data
+    # test_data, test_label = load_data_set(9,10)
+    test_data, test_label = load_test_norm()
+
+    X = test_data
+
+    Y = test_label
+    # print(Y)
+
+    _Y = mycls.predict(X)
+
+    error = np.sum(np.abs(Y-_Y)/2)
+    print("[correct rate:{:.5f}][test size:{}][error size:{}]".format(1 - error/len(Y),len(Y),error))
+
+    plt.plot(mycls.w)
+    plt.show()
+
+def train_lda():
+    train_data, train_label = load_train_norm()
+    print(train_data.shape)
+    print(train_label.shape)
+
+    data_pos = train_data[np.squeeze(train_label==1),:]
+    data_neg = train_data[np.squeeze(train_label == -1), :]
+
+    # print(data_pos.shape)
+    # print(data_neg.shape)
+
+    mu_pos = data_pos.mean(axis=0).reshape(-1,1)
+    mu_neg = data_neg.mean(axis=0).reshape(-1, 1)
+
+    delta_mu = mu_pos-mu_neg
+    # print(mu_pos.shape)
+    SB = np.matmul(delta_mu , delta_mu.T)
+
+    cov_pos = np.cov(data_pos.T)
+    cov_neg = np.cov(data_neg.T)
+    # print(cov_pos.shape)
+    SW = (len(data_pos)*cov_pos + len(data_neg)*cov_neg)
+    # SW = SW/len(train_data)
+
+    print(np.linalg.slogdet(SW))
+
+    landa,beta = np.linalg.eigh(np.matmul(np.linalg.inv(SW),SB),"U")
+
+    plt.plot(landa)
+    plt.show()
+    print(np.dot(beta[0], beta[1]), np.dot(beta[1], beta[2]))
+    # exit()
+
+    
+    
+    print(landa.max(),landa.min())
+    
+    print(beta.shape)
+    index = (np.abs(landa)).argmin()
+    indexs = np.argsort(np.abs(landa))
+
+    eig2 = np.matmul(np.linalg.inv(SW), delta_mu)
+
+    z = np.matmul(train_data, eig2)
+    # print(z.shape)
+    plt.hist(z[train_label == 1], bins=200, alpha=0.5, label="pos")
+    plt.hist(z[train_label == -1], bins=200, alpha=0.5, label="neg")
+    plt.legend()
+
+    plt.show()
+
+    for i in range(-1,-10,-1):
+        eig = beta[:, indexs[i]].reshape(-1, 1)
+        # print(eig.shape)
+
+        eig2 = np.matmul(np.linalg.inv(SW),delta_mu)
+
+        z = np.matmul(train_data,eig)
+        # print(z.shape)
+        plt.hist(z[train_label==1],bins=200,alpha=0.5,label="pos")
+        plt.hist(z[train_label == -1], bins=200, alpha=0.5, label="neg")
+        plt.legend()
+
+        plt.show()
+
+
+if __name__ == "__main__":
+    # train_logistic()
+    train_lda()
--- mymodel.py
+++ mymodel.py
@@ -1,12 +1,15 @@
 import numpy as np
+import torch.utils.data
 
 
 class LogisticClassifier(object):
-    def __init__(self,n_feature,LR):
+    def __init__(self,n_feature,vis=True ,batch_size = 64, LR = 0.001):
         self.n_feature = n_feature
         self.lr = LR
+        self.vis = vis
+        self.batch_size = batch_size
         self.w = np.random.normal(loc=0.0, scale=0.01, size=n_feature)
-    
+
     def learn(self,X,Y):    
         """
         X shape: n*p
@@ -26,9 +29,19 @@ class LogisticClassifier(object):
         # gradient = 1-
         gradient = np.matmul(np.divide(1, 1 + np.exp(p)).T, -np.multiply(Y, X))
         gradient = np.squeeze(gradient)
+        ## SGD
         self.w = self.w - self.lr*gradient
 
+        # ## Langevin  SGLD
+        # epsilon = np.random.normal(loc=0.0, scale=np.sqrt(self.lr), size=self.n_feature)
+        # self.w = self.w - 0.5*self.lr*gradient + np.sqrt(self.lr)*epsilon 
+
+        ## lr decay
+        # self.lr *=0.9999 
+
         return loss,acc
+
+
     def predict(self,X):
         _Y = 1/(1+np.exp(-np.matmul(X, self.w)))
         
